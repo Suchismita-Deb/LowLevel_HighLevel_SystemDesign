33d4148506a40c3e5c538fae252eab697eddba44
Suchismita-Deb <suchismitadeb2000@gmail.com>
2024-05-06 19:38:06 +0530

    Decorator Pattern Better Code Added.

--- src/main/java/org/example/DesignPattern/StructuralDesignPattern/BetterCode/MainApp.java
+++ src/main/java/org/example/DesignPattern/StructuralDesignPattern/BetterCode/MainApp.java
@@ -1,19 +1,12 @@
 package org.example.DesignPattern.StructuralDesignPattern.BetterCode;
 
 public class MainApp {
-
-    /*
-     * Video Reference: https://youtu.be/v6tpISNjHf8
-     */
     public static void main(String[] args) {
 
-        INotifier notifyAll = new FacebookDecorator(new WhatsAppDecorator(new Notifier("Geekific")));
-        notifyAll.send("Like and Subscribe!!!");
-
-        System.out.println("==========================================");
-
-        INotifier notifyFbMail = new FacebookDecorator(new Notifier("Geekific"));
-        notifyFbMail.send("Like and Subscribe!!!");
+        INotifier notifyAll = new FacebookDecorator(new WhatsAppDecorator(new Notifier("SDEB")));
+        notifyAll.send("Message");
+        INotifier notifyFbMail = new FacebookDecorator(new Notifier("SDEB"));
+        notifyFbMail.send("Message");
 
     }
 
